5cc0bbb2407a5978397c56aa045ed9c182b32c0d
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2026-03-04 16:05:16 +0530

    Fix SendGrid SMTP password configuration

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "sendgrid_backend",
+    # "sendgrid_backend",
     "rfp",
 ]
 
