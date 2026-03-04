3701d45af0726aa0165b6de0281428a55c65455a
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2026-03-04 19:33:07 +0530

    Fix SendGrid SMTP password configuration

--- config/settings.py
+++ config/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # --- Database (Render Postgres) ---
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR/'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=not DEBUG,
     )
 }
 
